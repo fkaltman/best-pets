4b611c83fdd992fbf45d9f23affaa888e8225a6f
Francine Altman <francinekline@gmail.com>
2025-11-21 11:33:28 -0500

    remove expo boilerplate

---
+++ app/index.tsx
@@ -0,0 +1,18 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Find the perfect pet</Text>
+
+
+    </View>
+  );
+}
+
